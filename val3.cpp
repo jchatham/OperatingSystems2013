7d5b0632397e237a6dba28e942cef210dc441d24
David Blake <davidblak3@Davids-MacBook-Pro.local>
2013-04-03 11:22:05 -0400

    Revert "k"
    This reverts commit 2b8e91065dab2809c8e454560df88d4a40bf4491.

---
+++ val3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+ using namespace std;
+ int main(void)
+ {
+   
+   int indx, stop;
+   int size = -9;
+   while(size <0)
+   {
+   cout<<"Enter the size of the array"<<"\n";
+   cin>>size;
+   cout<<"\n\n\n\n";
+   }
+   
+   int * vals;
+   vals = new int[size];
+   
+   indx = size;
+     
+   for (size_t i=0; i < size; i++)
+   {
+			vals[i]= size - i;
+			cout<< vals[i] <<"\n";
+   }
+   do
+   {
+   	cout<< "Which index item would you like to retrieve?\n";
+   	cin>> indx;
+   }
+   while(indx > size || indx < 0);
+   
+   cout<< "The value at position " << indx <<"\n";
+   cout<<"is: "<<  vals[indx];
+
+   cin>>stop;
+   return 0;
+ }
