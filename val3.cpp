caaf61b3a877c558364be04f0a68523ddb766091
kkhhaaaannnnnn <jeff.chatham@gmail.com>
2025-05-24 08:53:04 -0400

    Update val3.cpp
    Some cleanup and comments. I have no idea what college me was thinking.

--- val3.cpp
+++ val3.cpp
@@ -1,36 +1,39 @@
 #include <iostream>
  using namespace std;
- int main(void)
- {
-   //check check
-   int whatareudoingcloud9 = 0009993;
+ int main(void){
    int indx, stop;
    int size = -9;
+   int * vals;
+
+   //Checks size of aray.	 
    while(size <0)
    {
-   cout<<"Enter the size of the array"<<"\n";
-   cin>>size;
-   cout<<"\n\n\n\n";
+       cout<<"Enter the size of the array"<<"\n";
+       cin>>size;
+       cout<<"\n\n\n\n";
    }
-   
-   int * vals;
+
+
+    
+   //Initalizes array to user inputted size. 
    vals = new int[size];
    
    indx = size;
-     
-   for (size_t i=0; i < size; i++)
+
+   //Assigns the array values descending from size down to zero for the last position. I'm not certain why I used size_t here, but I haven't used C++ in a long time. It seems obvious that this would be better with an int data type.  
+   for (size_t i = 0; i < size; i++)
    {
-			vals[i]= size - i;
-			cout<< vals[i] <<"\n";
+        vals[i] = size - i;
+	cout<< vals[i] <<"\n";
    }
+
+   //Prints the value at an index. 	 
    do
    {
    	cout<< "Which index item would you like to retrieve?\n";
    	cin>> indx;
    }
    while(indx > size || indx < 0);
-   //here is a changed that I made
-   //lets see if you get this change
    cout<< "The value at position " << indx <<"\n";
    cout<<"is: "<<  vals[indx];
 
